71be18f5388866b730398fbc3b52a2e0d5397ecd
Emily Lin <116020783+Emily8183@users.noreply.github.com>
2024-09-27 23:45:01 -0500

    Update medium account name.js

--- route/api/medium.js
+++ route/api/medium.js
@@ -1,9 +1,9 @@
 const axios = require("axios");
-const mediumURL ="https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@emily.zy.lin";
+const mediumURL ="https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@";
 
 async function getArticlesByUsername(username) {
   try {
-    const result = await axios.get(mediumURL + username);
+    const result = await axios.get(mediumURL + emily.zy.lin);
     const filteredResult = result.data.items.filter(
       (item) =>
         item.categories.length > 0

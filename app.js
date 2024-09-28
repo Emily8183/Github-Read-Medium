e82e3a29aff944cb4aff782bca8f0ad6524d78dc
Emily <emily.zy.lin@gmail.com>
2024-09-28 16:21:31 -0500

    converting to netlify function

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 var port = process.env.PORT || 3000;
 app.use(express.json());
-app.use(require("./route/router"))
+app.use(require("./route/api"));
 
 app.listen(port, function () {
   console.log("Server listening " + port);
